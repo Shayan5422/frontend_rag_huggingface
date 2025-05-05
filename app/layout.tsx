00638c19251dd4a28a3c480fdc992f69194d355d
Shayan Hashemi <83245719+Shayan5422@users.noreply.github.com>
2025-05-05 21:20:38 +0200

    new analayic

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,12 @@ import type React from "react"
 import "@/app/globals.css"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@vercel/analytics/react"
+import type { Metadata } from "next"
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Model Search",
   description: "Search for models based on semantic similarity",
   generator: 'v0.dev',
@@ -22,9 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head />
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
